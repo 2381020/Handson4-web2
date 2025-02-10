761c7e7bd6e09a03f926c440ea27ea7f6729394f
Andrew Simbolon <2381020@unai.edu>
2025-02-10 11:03:21 +0700

    update handson4

---
+++ src/State/UsingState.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const UsingState = () => {
+    const [count, setCount] = useState(0);
+    console.log(count);
+    return (
+        <div>
+            <p>Counter Value: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+        </div>
+    )
+}
+
+export default UsingState
